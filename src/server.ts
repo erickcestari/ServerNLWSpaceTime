f3e149c2bcd66cdf8c1be114bdbb3c92dc012c5d
Erick <erickcestari03@gmail.com>
2023-05-24 17:23:09 -0300

    adding upload

--- src/server.ts
+++ src/server.ts
@@ -4,23 +4,34 @@ import cors from '@fastify/cors'
 import 'dotenv/config'
 import { authRoutes } from './routes/auth'
 import jwt from '@fastify/jwt'
+import Multipart from '@fastify/multipart'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
 
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads'
+})
+
+app.register(Multipart)
+
 app.register(cors, {
-  origin: ['http://localhost:3000'],
+  origin: true,
 })
 
 app.register(jwt, {
   secret: '120i3901kdj921j0d91kj9j1d90j921jkdskd'
 })
 
-app.register(memoriesRoutes)
 app.register(authRoutes)
+app.register(uploadRoutes)
+app.register(memoriesRoutes)
 app
   .listen({
     port: 3333,
-    host: '0.0.0.0',
+    host: '0.0.0.0'
   })
   .then(() => {
     console.log('🤖 HTTP server running on http://localhost:3333')
